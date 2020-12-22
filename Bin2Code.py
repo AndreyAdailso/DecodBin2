6793e51dc07a0c1aef39d4b401de7a9bc7562a1a
Andrey Adailso <andreyadailsom@gmail.com>
2020-12-22 16:07:46 -0300

    bin_convert melhorado tratamento de erro adicionado

--- Bin2Code.py
+++ Bin2Code.py
@@ -4,28 +4,33 @@
 def bin_convert(bin_str):
     """ Recebe uma string representando um binario e retorna o valor em int decimal."""
     decimal = 0
-    cont = 0 
     digt_bin = []
 
     for i in bin_str:
         digt_bin.append(int(i))
     
-    for index in range(len(digt_bin)-1, -1, -1):
-        n = digt_bin[index] * (2**cont)
+    for index in range(0, len(digt_bin)):
+        n = digt_bin[index] * (2**(len(digt_bin) - 1 - index)) 
         decimal = decimal + n
-        cont = cont + 1
         
     return decimal
-    
+
+def check(bin_str):
+    """Verifica se a string contem apenas 0 ou 1"""
+    for i in bin_str:
+        if i != '1' and i != '0':
+            return False
+    return True
+
 while True:     
     print("Digite o binario que deseja converter.")
     print(">>>", end='')
+    
     input_bin = str(input())
     
-    deci_bin = bin_convert(input_bin)
-
-    print(input_bin,"=", deci_bin)
-    print("FIM")
-    break
-
-
+    if check(input_bin) == True:
+        deci_bin = bin_convert(input_bin)
+        print(input_bin,"=", deci_bin)
+        break
+    else:
+        print("Por favor Digite um número binario valido.")
